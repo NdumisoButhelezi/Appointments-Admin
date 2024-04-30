e237c278668c0053d6c5d6941593a4e34248e0ac
NdumisoButhelezi <108745657+NdumisoButhelezi@users.noreply.github.com>
2024-04-30 19:03:08 +0200

    Initial commit

---
+++ app/src/main/java/com/example/appointments_admin/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.appointments_admin;
+
+import static com.example.appointments_admin.R.id.logout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+    Button button;
+    TextView textView;
+
+    FirebaseUser user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(logout);
+        textView = findViewById(R.id.user_details);
+        user = auth.getCurrentUser();
+
+        // Check if the user is not logged in
+        if (user == null) {
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
+            finish();
+        } else {
+            // Display the user's email
+            textView.setText(user.getEmail());
+        }
+
+        // Set up a click listener for the logout button
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Sign out from Firebase
+                auth.signOut();
+
+                // After signing out, redirect to the Login activity
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
